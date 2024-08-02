5ef07503723b83741fbdf1cd90caefbcd1425605
heCarlo <carlosgfkp@outlook.com>
2024-08-02 16:13:05 -0300

    feat: added home inventory page

---
+++ app/inventory/page.tsx
@@ -0,0 +1,13 @@
+// app/inventory/page.tsx
+import React from 'react';
+
+const Inventory: React.FC = () => {
+  return (
+    <div>
+      <h1>Controle de Estoque</h1>
+      <p>Aqui você pode gerenciar o estoque de peças e materiais.</p>
+    </div>
+  );
+};
+
+export default Inventory;
