2916912431bfe123bfbd4629a581751e2dc0a0dc
heCarlo <carlosgfkp@outlook.com>
2024-08-02 16:12:53 -0300

    feat: added cost-control page

---
+++ app/cost-control/page.tsx
@@ -0,0 +1,13 @@
+// app/cost-control/page.tsx
+import React from 'react';
+
+const CostControl: React.FC = () => {
+  return (
+    <div>
+      <h1>Controle de Custos</h1>
+      <p>Aqui você pode controlar os custos de manutenção.</p>
+    </div>
+  );
+};
+
+export default CostControl;
