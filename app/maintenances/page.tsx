62f51fc66cdba44a06d87f299678c7a80b9bbaa4
heCarlo <carlosgfkp@outlook.com>
2024-08-02 16:13:38 -0300

    feat: added maintenances page

---
+++ app/maintenances/page.tsx
@@ -0,0 +1,13 @@
+// app/maintenance/page.tsx
+import React from 'react';
+
+const Maintenance: React.FC = () => {
+  return (
+    <div>
+      <h1>Manutenções</h1>
+      <p>Aqui você pode gerenciar as manutenções.</p>
+    </div>
+  );
+};
+
+export default Maintenance;
