da4a4156b1c363eb9577a67fb2b04c3d520f0302
heCarlo <carlosgfkp@outlook.com>
2024-08-02 16:13:54 -0300

    feat: added teams page

---
+++ app/teams/page.tsx
@@ -0,0 +1,13 @@
+// app/teams/page.tsx
+import React from 'react';
+
+const Teams: React.FC = () => {
+  return (
+    <div>
+      <h1>Atribuição de Equipes</h1>
+      <p>Aqui você pode atribuir equipes às manutenções.</p>
+    </div>
+  );
+};
+
+export default Teams;
