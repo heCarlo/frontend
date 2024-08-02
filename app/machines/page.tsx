b880489492cc558514b931bed714ed2c558ee6ec
heCarlo <carlosgfkp@outlook.com>
2024-08-02 16:13:13 -0300

    feat: added home machines page

---
+++ app/machines/page.tsx
@@ -0,0 +1,13 @@
+// app/machines/page.tsx
+import React from 'react';
+
+const Machines: React.FC = () => {
+  return (
+    <div>
+      <h1>Máquinas</h1>
+      <p>Aqui você pode gerenciar as máquinas.</p>
+    </div>
+  );
+};
+
+export default Machines;
