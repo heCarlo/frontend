d7e5babdfdb9a3ee18d39dd3d8a4ee7228e56fe2
heCarlo <carlosgfkp@outlook.com>
2024-08-02 16:13:47 -0300

    feat: added settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,13 @@
+// app/settings/page.tsx
+import React from 'react';
+
+const Settings: React.FC = () => {
+  return (
+    <div>
+      <h1>Configurações</h1>
+      <p>Aqui você pode ajustar as configurações do sistema.</p>
+    </div>
+  );
+};
+
+export default Settings;
